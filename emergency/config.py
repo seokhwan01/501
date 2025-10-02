6d10e8619974a4114232c5e8a1e7976829074d8d
seokhwan01 <soho010526@gmail.com>
2025-10-02 19:04:44 +0900

    ip

--- emergency/config.py
+++ emergency/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     # MQTT 설정
-    MQTT_BROKER = "192.168.192.1"   # ✅ 여기에 브로커 IP만 바꾸면 됨(변경 필수!!!!!)
+    MQTT_BROKER = "10.210.98.208"   # ✅ 여기에 브로커 IP만 바꾸면 됨(변경 필수!!!!!)
     MQTT_PORT = 1883
 
     # 소켓 서버 주소
